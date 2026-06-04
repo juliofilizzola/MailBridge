dd4036e2c822f8a41b217fcb54e5d86a51db656b
julio_filizzola <juliofilizzola@hotmail.com>
2026-06-04 19:46:45 -0300

    feat: define SQS message payload interface
    details:
    - create `SQSMessagePayload` interface for standardizing SQS message structure
    - include `targetProvider` and `emailData` properties

---
+++ src/interface/SQS.ts
@@ -0,0 +1,7 @@
+import {Providers} from "@src/enuns/providers";
+import type {EmailPayload} from "@src/interface/types";
+
+export interface SQSMessagePayload {
+  targetProvider: Providers;
+  emailData: EmailPayload;
+}
