65e40b53ac500d7a3e74b5962968259bbbe9a4e4
julio_filizzola <juliofilizzola@hotmail.com>
2026-06-04 20:37:13 -0300

    feat: enhance SQS email worker with polling and message processing
    details:
    - add start and stop polling methods to manage queue processing state
    - implement message processing logic with error handling and dispatch
    - include utility for message deletion from the SQS queue

--- src/services/sqsEmailWorker.ts
+++ src/services/sqsEmailWorker.ts
@@ -1,13 +1,87 @@
-import type { SQS } from '@aws-sdk/client-sqs';
+import {
+  ReceiveMessageCommand,
+  DeleteMessageCommand,
+  type SQSClient,
+  type Message,
+} from '@aws-sdk/client-sqs';
 import type { MailDispatcherService } from '@src/services/MailDispatcherService';
+import {MAX_NUMBER_MESSAGE, WAIT_TIME_MESSAGE} from "@src/commons/constants";
 
 export class SqsEmailWorker {
-  private sqsClient: SQS;
-  private queueUrl: string;
+  private sqsClient: SQSClient;
+  private readonly queueUrl: string;
   private mailDispatcher: MailDispatcherService;
-  constructor(sqsClient: SQS, queueUrl: string, mailDispatcher: MailDispatcherService) {
+  private isPollingActive: boolean;
+  constructor(sqsClient: SQSClient, queueUrl: string, mailDispatcher: MailDispatcherService) {
     this.sqsClient = sqsClient;
     this.queueUrl = queueUrl;
     this.mailDispatcher = mailDispatcher;
+    this.isPollingActive = false;
+  }
+
+  public async startPolling(): Promise<void> {
+    this.isPollingActive = true;
+
+    while (this.isPollingActive) {
+      await this.processQueue();
+    }
+  }
+
+
+  public stopPolling(): void {
+    this.isPollingActive = false;
+  }
+
+  public async processQueue(): Promise<void> {
+    try {
+      const receiveMessageCommand = new ReceiveMessageCommand({
+        QueueUrl: this.queueUrl,
+        MaxNumberOfMessages: MAX_NUMBER_MESSAGE,
+        WaitTimeSeconds: WAIT_TIME_MESSAGE,
+      });
+
+      const response = await this.sqsClient.send(receiveMessageCommand);
+
+      if (!response.Messages){
+        return;
+      }
+
+
+      for (const message of response.Messages) {
+
+      }
+
+
+    } catch (error) {
+      console.error('Error processing SQS queue:', error);
+    }
+  }
+
+  private async processMessage(message: Message): Promise<void> {
+    try {
+      if (!message.Body) {
+        await this.deleteMessage(message.ReceiptHandle);
+        return;
+      }
+
+      const parseMessage = JSON.parse(message.Body);
+      await this.mailDispatcher.dispatchEmail(parseMessage.emailData);
+      await this.deleteMessage(message.ReceiptHandle);
+    } catch (error) {
+      console.error('Error processing SQS message:', error);
+    }
+  }
+
+  private async deleteMessage(messageRecipeHandle: string | undefined) {
+    if (!messageRecipeHandle) {
+      return;
+    }
+
+    const deleteMessageCommand = new DeleteMessageCommand({
+      QueueUrl: this.queueUrl,
+      ReceiptHandle: messageRecipeHandle,
+    });
+
+    await this.sqsClient.send(deleteMessageCommand);
   }
 }
