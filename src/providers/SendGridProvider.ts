f8f932b84ea8b16e02111b841a6527a7a0cc57ea
julio_filizzola <juliofilizzola@hotmail.com>
2026-06-04 19:58:07 -0300

    style: format imports and constructor in SendGridProvider
    details:
    - adjust import spacing and type declaration for consistency
    - simplify formatting for the constructor method

--- src/providers/SendGridProvider.ts
+++ src/providers/SendGridProvider.ts
@@ -1,11 +1,9 @@
-import type {EmailPayload, EmailProvider} from "@src/interface/types";
 import sgMail from '@sendgrid/mail';
+import type { EmailPayload, EmailProvider } from '@src/interface/types';
 
-export class sendGridProvider implements  EmailProvider {
+export class sendGridProvider implements EmailProvider {
   constructor(apiKey: string) {
-    sgMail.setApiKey(
-      apiKey
-    );
+    sgMail.setApiKey(apiKey);
   }
 
   public async sendEmail(payload: EmailPayload): Promise<void> {
