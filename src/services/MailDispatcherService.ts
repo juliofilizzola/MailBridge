8221efca417a6a081c90ff5438a38c569623edf3
julio_filizzola <juliofilizzola@hotmail.com>
2026-06-04 22:24:05 -0300

    refactor: enhance email dispatch logic with provider registry
    details:
    - replace single email provider with a registry supporting multiple providers
    - add constructor to initialize provider registry map
    - update dispatchEmail method for dynamic provider selection

--- src/services/MailDispatcherService.ts
+++ src/services/MailDispatcherService.ts
@@ -1,13 +1,20 @@
-import type { EmailPayload, EmailProvider } from '@src/interface/types';
+import { Providers } from '@src/enums/providers';
+import type { SQSMessagePayload } from '@src/interface/SQS';
+import type { EmailProvider } from '@src/interface/types';
 
 export class MailDispatcherService {
-  private activeEmailProvider: EmailProvider;
+  private readonly providerRegistry: Map<Providers, EmailProvider>;
 
-  constructor(emailProvider: EmailProvider) {
-    this.activeEmailProvider = emailProvider;
+  constructor(providerRegistry: Map<Providers, EmailProvider>) {
+    this.providerRegistry = providerRegistry;
   }
 
-  public async dispatchEmail(payload: EmailPayload): Promise<void> {
-    await this.activeEmailProvider.sendEmail(payload);
+  public async dispatchEmail(payload: SQSMessagePayload): Promise<void> {
+    const selectProviderInstance = this.providerRegistry.get(payload.targetProvider);
+    if (!selectProviderInstance) {
+      throw new Error('Provider not found');
+    }
+
+    await selectProviderInstance.sendEmail(payload.emailData);
   }
 }
